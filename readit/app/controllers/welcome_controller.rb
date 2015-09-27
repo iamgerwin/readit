9eefd1973b43587f9853d68f1bf0e3531e22692f
John Gerwin De las Alas <iamgerwin@aim.com>
2015-09-27 22:00:14 +0800

    Experiment for session, flash and cookies

--- readit/app/controllers/welcome_controller.rb
+++ readit/app/controllers/welcome_controller.rb
@@ -3,5 +3,8 @@ class WelcomeController < ApplicationController
     @mmm = "Jane"
     @@var = "John"
     @v = @@var + " w".upcase
+    session[:name] = "Name session here"
+    flash[:name] = "Name flash here"
+    cookies[:name] = "Name cookies here"
   end
 end
